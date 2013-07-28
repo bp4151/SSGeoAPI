b8793541e65e5c4a65475631097e15951cc243bb
jaxmeier <bp4151@gmail.com>
2013-07-28 10:54:05 -0400

    Changed location isInPlace business logic

--- Services/LocationService.cs
+++ Services/LocationService.cs
@@ -54,7 +54,12 @@ namespace GeoAPI
 
 			Location location = new Location ();
 			location.user_id = request.user_id;
-			location.create_date = request.create_date;
+			if (request.create_date == DateTime.MinValue) {
+				location.create_date = DateTime.Now;
+			} else {
+				location.create_date = request.create_date;
+			}
+
 			location.loc = new GeoJson2DGeographicCoordinates (request.longitude, request.latitude);
 
 			//Insert the location into the locations collection
@@ -78,6 +83,9 @@ namespace GeoAPI
 			//if EXIT, then run exit trigger where placeID = placeID and type = EXIT
 
 			foreach (KeyValuePair<ObjectId, string> de in dictPlaces) {
+				//Update the place by placeID with the userID based on the type
+				//if ENTER, add the user to the place.usersInPlace dictionary
+				//if EXIT, remove the user from the place.usersInPlace dictionary
 				//RunTrigger (de.Key, de.Value);
 			}
 
@@ -88,6 +96,11 @@ namespace GeoAPI
 			return response;
 		}
 
+		/// <summary>
+		/// Gets the places by location.
+		/// </summary>
+		/// <returns>The places by location.</returns>
+		/// <param name="request">Request.</param>
 		Dictionary<ObjectId, string> GetPlacesByLocation (LocationRequest request)
 		{
 			var earthRadius = 6378.0; // km
@@ -98,39 +111,102 @@ namespace GeoAPI
 			MongoCollection<PlaceResponse> placescollection = db.GetCollection<PlaceResponse> ("place");
 			MongoCollection<Location> locationscollection = db.GetCollection<Location> ("location");
 
+			if (!BsonClassMap.IsClassMapRegistered (typeof(PlaceResponse))) {
+				BsonClassMap.RegisterClassMap<PlaceResponse> ();
+			}
+			if (!BsonClassMap.IsClassMapRegistered (typeof(Location))) {
+				BsonClassMap.RegisterClassMap<Location> ();
+			}
+
 			placescollection.EnsureIndex (IndexKeys.GeoSpatialSpherical ("loc"));
 
-			foreach (PlaceResponse place in placescollection.FindAll ()) {
-				var icquery = Query.WithinCircle ("loc", place.loc.Longitude, place.loc.Latitude, place.radius / earthRadius, true);
+			foreach (var place in placescollection.FindAll ()) {
+
+				//Set the query to determine if the new location is in each place
+				var icquery = Query.WithinCircle ("loc", place.loc.Longitude, place.loc.Latitude, place.radius / earthRadius, false);
+
+				//Debug
+				Console.WriteLine (icquery);
 
 				//Get all locations for user by createdate desc
 				var locs = locationscollection.FindAll ().Where (u => u.user_id == request.user_id).OrderByDescending (l => l.create_date);
 				//Get all locations inside place for user by createdate desc
 				var locsinsideplace = locationscollection.Find (icquery).Where (l => l.user_id == request.user_id).OrderByDescending (l => l.create_date);
 
-				List<Location> listlocs = locs.ToList ();
-				List<Location> listlocsinsideplace = locsinsideplace.ToList ();
+				List<Location> listlocs = new List<Location> ();
+				List<Location> listlocsinsideplace = new List<Location> ();
 
-				//If the list of locations inside the place has a count of 1, then the user is new to the place
-				if (listlocsinsideplace.Count == 1) {
-					dictPlaces.Add (place.Id, "ENTER");
-					//If an enter trigger exists on the place, fire the trigger and add the user to the place
-					Console.WriteLine ("Execute ENTER trigger");
+				//get the total list of locations for the given user into a list
+				listlocs = locs.ToList ();
+
+				//get the list of locations that are in this place
+				listlocsinsideplace = locsinsideplace.ToList ();
+
+				int idxCurrentLocation = -1;
+				int idxPriorLocation = -1;
+
+				idxCurrentLocation = listlocsinsideplace.FindIndex (l => l.Id == listlocs [0].Id);
+				Console.WriteLine ("idxCurrentLocation: " + idxCurrentLocation);
+
+				if (listlocs.Count > 1) {
+					idxPriorLocation = listlocsinsideplace.FindIndex (l => l.Id == listlocs [1].Id);
+					Console.WriteLine ("idxPriorLocation: " + idxPriorLocation);
 				}
-				//If the list of locations inside the place has a count > 1, and the id of the location inside the place is the most current, and the second location is not inside the place, then the user has entered the place
-				else if (listlocsinsideplace.Count > 1 && listlocs [0].Id == listlocsinsideplace [0].Id && listlocs [1].Id == listlocsinsideplace [0].Id) {
+				//if idxCurrentLocation = -1 and idxPriorLocation = -1 location not in place
+				if (idxCurrentLocation == -1 && idxPriorLocation == -1) {
+					Console.WriteLine ("Not in place");
+				}
+				//if idxCurrentLocation = 0 and idxPriorLocation = -1 ENTER
+				else if (idxCurrentLocation == 0 && idxPriorLocation == -1) {
 					dictPlaces.Add (place.Id, "ENTER");
 					//If an enter trigger exists on the place, fire the trigger and add the user to the place
+					place.usersInPlace.AddIfNotExists (request.user_id);
+					placescollection.Save (place);
+					Console.WriteLine ("Execute ENTER trigger");
+				} 
+				//if idxCurrentLocation = -1 and idxPriorLocation = 0 EXIT
+				else if (idxCurrentLocation == -1 && idxPriorLocation == 0) {
+					dictPlaces.Add (place.Id, "EXIT");
+					//If an exit trigger exists on the place, fire the trigger and remove the user to the place
+					place.usersInPlace.Remove (request.user_id);
+					placescollection.Save (place);
+					Console.WriteLine ("Execute EXIT trigger");
+				}
+				//if idxCurrentLocation = 0 and idxPriorLocation >= 0 STILL IN PLACE
+				else if (idxCurrentLocation == 0 && idxPriorLocation >= 0) {
+					Console.WriteLine ("Still in place");
+				}
+
+
+
+				/*
+				//If the list of locations inside the place has a count > 1, and the id of the location inside the place is the most current, and the second location is not inside the place, then the user has entered the place
+				if (listlocsinsideplace.Count >= 1 && listlocs.Count > 1 && listlocs [0].Id == listlocsinsideplace [0].Id && listlocs [1].Id == listlocsinsideplace [0].Id) {
+					dictPlaces.Add (place.Id, "ENTER");
+					//If an enter trigger exists on the place, fire the trigger and add the user to the place
+					place.usersInPlace.AddIfNotExists (request.user_id);
+					placescollection.Save (place);
 					Console.WriteLine ("Execute ENTER trigger");
 				}
 				//If the list of locations inside the place has a count > 1, and the id of the location inside the place is the second location, and the first location is not inside the place, then the user has left the place
-				else if (listlocsinsideplace.Count > 1 && listlocs [1].Id == listlocsinsideplace [0].Id && listlocs [0].Id != listlocsinsideplace [0].Id) {
+				else if (listlocsinsideplace.Count >= 1 && listlocs.Count > 1 && listlocs [1].Id == listlocsinsideplace [0].Id && listlocs [0].Id != listlocsinsideplace [0].Id) {
 					dictPlaces.Add (place.Id, "EXIT");
 					//If an exit trigger exists on the place, fire the trigger and remove the user to the place
+					place.usersInPlace.Remove (request.user_id);
+					placescollection.Save (place);
 					Console.WriteLine ("Execute EXIT trigger");
+				} 
+				//If the list of locations inside the place has a count of 1, then the user is new to the place
+				else if (listlocsinsideplace.Count == 1) {
+					dictPlaces.Add (place.Id, "ENTER");
+					//If an enter trigger exists on the place, fire the trigger and add the user to the place
+					place.usersInPlace.AddIfNotExists (request.user_id);
+					placescollection.Save (place);
+					Console.WriteLine ("Execute ENTER trigger");
 				} else {
 					Console.WriteLine ("Still in place");
 				}
+				*/
 
 			}
 
